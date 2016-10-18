021f02dd72bc735528b8e7a3de8f3d256b3018a5
Mayorov Maxim <nub.pun@yandex.ru>
2016-10-18 18:05:27 +0500

    Complete first task

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace Generic
     {
         static void Main(string[] args)
         {
+            var v1 = Processor.CreateEngine<Car>().For<Car2>().With<Car2>();
+            var v2 = Processor.CreateEngine<Double>().For<Car2>().With<Car2>();
+        }
+
+        class Car
+        {
+
+        }
+
+        class Car2
+        {
+
         }
     }
 }
