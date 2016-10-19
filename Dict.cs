086d0ebf3abbcf9d889bb0e373dcdad567cc128e
Mayorov Maxim <nub.pun@yandex.ru>
2016-10-19 21:04:24 +0500

    task2

---
+++ Dict.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Dict
+    {
+        Dictionary<Guid, object> dict;
+        Dictionary<Type, List<Guid>> dictCollection;
+
+        public Dict()
+        {
+            dict = new Dictionary<Guid, object>();
+            dictCollection = new Dictionary<Type, List<Guid>>();
+        }
+
+        public T Add<T>(T item)
+        {
+            Guid guid = Guid.NewGuid();
+            dict[guid] = item;
+
+            if (!dictCollection.ContainsKey(item.GetType()))
+            {
+                dictCollection.Add(item.GetType(), new List<Guid>());
+            }
+
+            dictCollection[item.GetType()].Add(guid);
+            return item;
+        }
+
+        public object GetObjByGuid(Guid guid)
+        {
+            return dict[guid];
+        }
+
+        public List<Tuple<Guid, object>> GetAll(Type type)
+        {
+            List<Tuple<Guid, object>> list = new List<Tuple<Guid, object>>();
+            foreach (var v in dictCollection[type])
+            {
+                Tuple<Guid, object> tp = new Tuple<Guid, object>(v, dict[v]);
+                list.Add(tp);
+            }
+            return list;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,17 +10,47 @@ namespace Generic
     {
         static void Main(string[] args)
         {
-            var v1 = Processor.CreateEngine<Car>().For<Car2>().With<Car2>();
-            var v2 = Processor.CreateEngine<Double>().For<Car2>().With<Car2>();
+            Dict test = new Dict();
+            test.Add(new Car2());
+            test.Add(new Car());
+            test.Add(new Car());
+            test.Add(new Car());
+            test.Add(new Car());
+            test.Add(new Car2());
+            Car2 c2 = new Car2();
+            Car c1 = new Car();
+            foreach (var v in test.GetAll(c1.GetType()))
+            {
+                Console.WriteLine(v.Item1 + " " + v.Item2);
+            }
         }
 
         class Car
         {
-
+            static int count = 0;
+            public Car()
+            {
+                count++;
+            }
+            override
+            public string ToString()
+            {
+                return "Car " + count;
+            }
         }
 
         class Car2
         {
+            static int count = 0;
+            public Car2()
+            {
+                count++;
+            }
+            override
+            public string ToString()
+            {
+                return "Car2 " + count;
+            }
 
         }
     }
